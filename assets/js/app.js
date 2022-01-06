938d3095cfbcd37fbb74edb917e3e4b407621cb6
Christina <92381241+kriscat@users.noreply.github.com>
2022-01-06 15:15:29 +0500

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,60 @@
+while (1) {
+    let a = +prompt("Выберите задание (1-3)");
+
+    switch (a) {
+        case 1:
+            {
+                alert("Найти сумму серии чисел от 1 до n.");
+                let n = +prompt("Задание 1: Введите ваше число, чтобы найти сумму серии чисел от 1 до вашего числа (n)");
+                let summa = 0;
+                for (let i = 1; i <= n; i++) {
+                    summa += i;
+                    alert(`Результат: ${summa}`);
+                }
+                break;
+            };
+
+        case 2:
+            {
+                alert("Найти умножение чисел от n до m");
+                let firstNum = +prompt("Задание 2: Введите первое число (n)");
+                let secondNum = +prompt("Задание 2: Введите второе число (m)");
+                let total = 1;
+                let i = 1;
+                for (i; i <= secondNum; i++) {
+                    alert(`Результат: ${total*=i}`);
+                }
+                for (i; i <= firstNum; i++) {
+                    alert(`Результат: ${total*=i}`);
+                }
+                break;
+            };
+        case 3:
+            {
+                alert("Найдите сумму серии Фибоначчи от 0 до n числа.");
+                let n = +prompt("Введите число n");
+                let numOne = 1;
+                let numTwo = 1;
+                let i = 0;
+                while (i < n - 2) {
+                    let numSum = numOne + numTwo;
+                    numOne = numTwo;
+                    numTwo = numSum;
+                    i = i + 1;
+                    console.log(`Результат: ${numTwo}`);
+                }
+                break;
+
+            };
+
+
+
+        default:
+            if (a == "" || a == null) {
+                alert("Отмена");
+            } else {
+                alert("Выберите еще раз задание");
+            }
+            break;
+    }
+}
